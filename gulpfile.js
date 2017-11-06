f8348e9afded7b8f25ba512bd9e04d673d2160d2
TiagoSaldanha <tiagosaldanha2016@outlook.com>
2017-11-06 15:16:03 -0200

    Configuração do gulpfile.js para compilar o sass apenas com o comando gulp no terminal

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Variables
+var prod = { outputStyle: 'compressed'};
+var dev = { outputStyle: 'expanded'};
+
+// Requires
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+
+// Task Default gulp
+gulp.task('default', ['sass', 'dev', 'watch'] );
+
+// Task para o Sass em ambiente de produção
+gulp.task('sass', function(){
+	return gulp.src('./sass/**/*.sass')
+			.pipe(sass(prod))
+			.on('error', sass.logError)
+			// .pipe(rename('index.min.css'))
+			.pipe(gulp.dest('./css/min'))
+});
+
+// Task para o Sass em ambiente de desenvolvimento
+gulp.task('dev', function(){
+	return gulp.src('./sass/**/*.sass')
+			.pipe(sass(dev))
+			.on('error', sass.logError)
+			.pipe(gulp.dest('./css'))
+});
+
+// Task para o Watch
+gulp.task('watch', function(){
+	gulp.watch('./sass/**/*.sass', ['sass', 'dev'])
+});
